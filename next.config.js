b676d2a34db7399a5d3f284b3eb887ec0d8616d6
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2022-03-11 20:46:30 -0300

    Added graphCMS image domain & authToken

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com'],
+    domains: ['images.unsplash.com', 'media.graphcms.com'],
   },
   env: {
     GRAPHCMS_ENDPOINT: process.env.GRAPHCMS_ENDPOINT,
+    AUTH_TOKEN: process.env.AUTH_TOKEN,
   }
 }
 
