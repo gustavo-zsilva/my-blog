8cd252f25c4f51bad73a0d4de0edfe447190b6dd
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2022-03-11 20:51:33 -0300

    urql config

--- src/lib/urql.ts
+++ src/lib/urql.ts
@@ -3,11 +3,16 @@ import { cacheExchange, createClient, dedupExchange, fetchExchange, ssrExchange 
 const endpoint = process.env.GRAPHCMS_ENDPOINT
 
 const isServerSide = typeof window === 'undefined'
-const ssrCache = ssrExchange({ isClient: !isServerSide })
+const ssrCache = ssrExchange({ isClient: !isServerSide, initialState: !isServerSide ? window.__URQL_DATA__: undefined })
 
 const client = createClient({
     url: String(endpoint),
-    exchanges: [dedupExchange, cacheExchange, ssrCache, fetchExchange],
+    exchanges: [
+        dedupExchange,
+        cacheExchange,
+        ssrCache,
+        fetchExchange
+    ],
 })
 
 export { client, ssrCache }
