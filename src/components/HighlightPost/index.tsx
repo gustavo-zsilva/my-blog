eb0ccfc468c0e4a8dc6b799a72edd6b63b2a18c6
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2022-03-16 19:27:39 -0300

    Finished HighlightPost

---
+++ src/components/HighlightPost/index.tsx
@@ -0,0 +1,66 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+import { formatDistance } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+
+import { FaFire } from 'react-icons/fa'
+import { Container, LatestBadge } from "./styles";
+
+type PostProps = {
+    id: string,
+    title: string,
+    slug: string,
+    readingTime: string,
+    publishedAt: string,
+    createdBy: {
+        name: string,
+        picture: string,
+    },
+    thumbnail: {
+        url: string,
+    }
+}
+
+export function HighlightPost(props: PostProps) {
+    const formattedDate = formatDistance(
+        new Date(props.publishedAt),
+        new Date(),
+        { locale: ptBR, addSuffix: true }
+    )
+
+    return (
+        <Link href={props.slug || '/'} passHref>
+            <Container css={{
+                backgroundImage: `url(${props.thumbnail.url})`
+            }}>
+                <div className="post-header">
+                    <div>
+                        <div>
+                            <picture>
+                                <Image
+                                    src={props.createdBy.picture}
+                                    alt="Author Picture"
+                                    width={30}
+                                    height={30}
+                                    objectFit="cover"
+                                />
+                            </picture>
+                            <span>{props.createdBy.name}</span>
+                        </div>
+                        <LatestBadge>
+                            <FaFire />
+                            <span>Latest</span>
+                        </LatestBadge>
+                    </div>
+                    <strong>{props.title}</strong>
+                </div>
+                <div>
+                    <span>{formattedDate}</span>
+                    <span>•</span>
+                    <span>{props.readingTime}</span>
+                </div>
+            </Container>
+        </Link>
+    )
+}
---
+++ src/components/HighlightPost/styles.ts
@@ -0,0 +1,96 @@
+import { styled } from '../../styles/stitches.config'
+
+export const Container = styled('button', {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+
+    padding: '2.2rem 1.8rem',
+    borderRadius: '.8rem',
+    boxShadow: '$post',
+
+    color: '#FFF',
+    textAlign: 'start',
+
+    gridColumn: '1 / 3',
+
+    backgroundImage: 'url(https://images.unsplash.com/photo-1647311955462-d4e8a725e5c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1509&q=80)',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+
+    position: 'relative',
+    overflow: 'hidden',
+    
+    '&:after': {
+        content: '',
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        right: 0,
+        left: 0,
+        background: 'rgba(0, 0, 0, 0.18)',
+    },
+
+    '.post-header': {
+        display: 'flex',
+        flexDirection: 'column',
+        zIndex: 1,
+
+        '> div': {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+
+            marginBottom: '$1',
+
+            'div': {
+                alignItems: 'center',
+
+                'span': {
+                    fontSize: '1rem',
+                },
+    
+                'picture': {
+                    display: 'flex',
+                    alignItems: 'center',
+    
+                    overflow: 'hidden',
+                    borderRadius: '$round',
+    
+                    border: '3px solid $primary',
+                    padding: '.1rem',
+                    
+                    'img': {
+                        borderRadius: '$round',
+                    },
+                },
+            },
+        },
+
+        'strong': {
+            fontSize: '2.2rem',
+        },
+    },
+
+
+    'div': {
+        display: 'flex',
+        gridGap: '.6rem',
+        zIndex: 1,
+
+        '> span': {
+            fontSize: '.875rem',
+        },
+    },
+})
+
+export const LatestBadge = styled('span', {
+    display: 'flex',
+    alignItems: 'center',
+    gridGap: '.4rem',
+
+    background: '$primary',
+    padding: '.4rem .8rem',
+    borderRadius: '$full',
+})
