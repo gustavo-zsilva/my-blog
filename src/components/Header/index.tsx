fa2e644263010769a29c325b96f145f56c2566db
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2022-03-16 19:39:03 -0300

    Trying to make react-aria work

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,10 +3,13 @@ import Link from 'next/link'
 import { BsFillMoonFill, BsFillSunFill } from 'react-icons/bs'
 
 import { useTheme } from '../../hooks/useTheme';
+import { useButton } from '@react-aria/button'
+
 import { Container, ThemeSwitch } from "./styles";
 
 export function Header() {
     const { handleSwitchTheme, theme } = useTheme()
+    // const {  } = useButton()
 
     return (
         <Container>
@@ -14,7 +17,9 @@ export function Header() {
                 <h2>My <span>Dev</span> Blog</h2>
             </Link>
             <div>
-                <ThemeSwitch onClick={handleSwitchTheme}>
+                <ThemeSwitch
+                    onClick={handleSwitchTheme}
+                >
                     {theme.className === 'dark-theme'
                         ? <BsFillSunFill color="#B4B4B4" size={14} />
                         : <BsFillMoonFill color="#111" size={14} />
