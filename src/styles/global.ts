a4ec96637771d0695fc5d7da181a57605cdb7962
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2022-03-25 17:15:32 -0300

    Styled Scrollbar

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,6 +9,21 @@ export const globalStyles = globalCss({
     'body, html': {
         fontFamily: 'Source Sans Pro, Helvetica, Arial, sans-serif',
     },
+    'html::-webkit-scrollbar': {
+        width: '.5vw',
+        padding: '.2rem'
+    },
+    'html::-webkit-scrollbar-thumb': {
+        backgroundColor: '#3335',
+        borderRadius: '$full',
+
+        '&:hover': {
+            backgroundColor: '#333',
+        }
+    },
+    'html::-webkit-scrollbar-track': {
+        backgroundColor: '#111',
+    },
     'button': {
         border: 0,
         fontFamily: 'inherit',
