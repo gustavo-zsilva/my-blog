741bfc874e33e8a3481fb3208a11e220f906267b
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2022-03-30 15:18:04 -0300

    Finished Level && exp functionalities

--- src/contexts/LevelContext.tsx
+++ src/contexts/LevelContext.tsx
@@ -1,39 +1,94 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useState, useEffect } from "react";
 
 export const LevelContext = createContext({} as LevelContextProps)
 
 type LevelContextProps = {
     level: number,
-    experience: number,
+    currentExperience: number,
+    experiencePercent: number,
+    finishedPosts: Post[],
     handleLevelUp: () => void,
     handleAddExperience: (amount: number) => void,
+    handleAddFinishedPost: (id: string, expAmount: number) => void,
 }
 
 type LevelProviderProps = {
     children: ReactNode,
 }
 
+type StorageLevel = {
+    level: number,
+    exp: number,
+}
+
+type Post = {
+    id: string,
+    expAmount: number,
+}
+
 export function LevelProvider({ children }: LevelProviderProps) {
     const [level, setLevel] = useState(1)
-    const [experience, setExperience] = useState(0)
+    const [currentExperience, setCurrentExperience] = useState(0)
+    const [finishedPosts, setFinishedPosts] = useState<Post[]>([])
+
+    const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
+    const experiencePercent = (currentExperience / experienceToNextLevel) * 100
+
+    useEffect(() => {
+        const savedLevel = localStorage.getItem('mdb_userLevel')
+        const parsedLevel: StorageLevel | null = savedLevel && JSON.parse(savedLevel)
+
+        const savedFinishedPosts = localStorage.getItem('mdb_finishedPosts')
+        const parsedFinishedPosts: Post[] | null = savedFinishedPosts && JSON.parse(savedFinishedPosts)
+        
+        setLevel(parsedLevel?.level ?? 1)
+        setCurrentExperience(parsedLevel?.exp ?? 0)
+        setFinishedPosts(parsedFinishedPosts ?? [])
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('mdb_userLevel', JSON.stringify({ level, exp: currentExperience }))
+        localStorage.setItem('mdb_finishedPosts', JSON.stringify(finishedPosts))
+    }, [currentExperience, level, finishedPosts])
 
     function handleLevelUp() {
         setLevel((prevState) => prevState + 1)
     }
 
     function handleAddExperience(amount: number) {
-        // Adicionar exp
-        // Maior que necessário para upar: Subir de nível e adicionar ao proximo
-        // Menor que necessário para upar: Apenas adicionar
+        // Somar exp
+        const experienceTotal = amount + currentExperience
+
+        // Maior ou igual ao necessário para upar: Subir de nível e adicionar exp
+        if (experienceTotal >= experienceToNextLevel) {
+            handleLevelUp()
+            setCurrentExperience(experienceTotal - experienceToNextLevel)
+            return
+        }
+
+        // Menor que necessário para upar: Setar para o total
+        setCurrentExperience(experienceTotal)
+    }
+
+    function handleAddFinishedPost(id: string, expAmount: number) {
+        const newFinishedPost = { id, expAmount }
+
+        if (finishedPosts.some((post) => post.id === id)) return
+
+        setFinishedPosts([...finishedPosts, newFinishedPost])
+        handleAddExperience(expAmount)
     }
 
     return (
         <LevelContext.Provider
             value={{
                 level,
-                experience,
+                currentExperience,
+                experiencePercent,
+                finishedPosts,
                 handleLevelUp,
                 handleAddExperience,
+                handleAddFinishedPost,
             }}
         >
             {children}
